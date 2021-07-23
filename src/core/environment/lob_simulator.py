3a7ee6f605d096d74c35e389d91c8307777fc676
authc <auth@zhaw.ch>
2021-07-23 23:40:43 +0200

    Providing a preliminary datafeed until real feed is done

--- src/core/environment/lob_simulator.py
+++ src/core/environment/lob_simulator.py
@@ -1,6 +1,7 @@
 import json
 from decimal import Decimal
 import random
+from src.core.environment.orderbook import OrderBook
 
 
 def split_book_to_orders(current_book):
@@ -40,26 +41,48 @@ def split_book_to_orders(current_book):
     return bid_orders, ask_orders, all_orders
 
 
-class LOBObserver:
-    def __init__(self, file_name, past_n, future_n):
+def raw_to_order_book(current_book, depth):
+    order_book = OrderBook()
+    bid_orders, ask_orders, _ = split_book_to_orders(current_book)
+
+    counter = 1
+    for bids in bid_orders:
+        _, _ = order_book.process_order(bids, False, False)
+        counter += 1
+        if counter > depth:
+            break
+
+    counter = 1
+    for asks in ask_orders:
+        _, _ = order_book.process_order(asks, False, False)
+        counter += 1
+        if counter > depth:
+            break
+    return order_book
+
+
+class LOBSimulator:
+    def __init__(self, file_name, nr_of_lobs, nr_of_lob_levels):
         self.file_name = file_name
-        self.past_n = past_n
-        self.future_n = future_n
+        self.nr_of_lobs = nr_of_lobs
+        self.nr_of_lob_levels = nr_of_lob_levels
 
         with open(self.file_name) as f:
             for i, l in enumerate(f):
                 pass
         self.line_count = i + 1
+        self.hist_count = 0
 
     def _reset_observer(self):
         self.hist_orders = []
         self.fut_orders = []
+        self.hist_count = 0
 
-    def draw_random_observations(self):
+    def _draw_random_observations(self, past_n, future_n):
 
         self._reset_observer()
-        # random integer from 0 to 9
-        rand_idx = random.randint(self.past_n, self.line_count - self.future_n)
+
+        rand_idx = random.randint(past_n, self.line_count - future_n)
 
         line_counter_hist = 1
         line_counter_fut = 1
@@ -67,25 +90,47 @@ class LOBObserver:
         fut_orders = []
         with open(self.file_name, "r+") as fp:
             for i, line in enumerate(fp):
-                if i == rand_idx - self.past_n - 1 + line_counter_hist and line_counter_hist <= self.past_n:
+                if i == rand_idx - past_n - 1 + line_counter_hist and line_counter_hist <= past_n:
                     read_data = json.loads(line)
                     hist_orders.append(read_data)
                     line_counter_hist += 1
-                elif i == rand_idx - 1 + line_counter_fut and line_counter_fut <= self.future_n:
+                elif i == rand_idx - 1 + line_counter_fut and line_counter_fut <= future_n:
                     read_data = json.loads(line)
                     fut_orders.append(read_data)
                     line_counter_fut += 1
-                elif i > rand_idx + self.future_n:
+                elif i > rand_idx + future_n:
                     break
         self.hist_orders = hist_orders
         self.fut_orders = fut_orders
         return self
 
+    def lob_snapshot(self, past_n, future_n, depth):
+        self._draw_random_observations(past_n=past_n, future_n=future_n)
+        book_hist = []
+        for i in range(0,len(self.hist_orders)):
+            book_hist.append(raw_to_order_book(self.hist_orders[i], depth=depth))
+        return book_hist
+
+    def next_lob_snapshot(self, depth, previous_lob_snapshot=None):
+        if self.hist_count >= len(self.fut_orders):
+            print('Cannot return anything beyond observed values. Need to call "lob_snapshot()" again!')
+            return
+        next_book = raw_to_order_book(self.fut_orders[self.hist_count], depth)
+        self.hist_count += 1
+        return next_book
+
 
 if __name__ == '__main__':
 
     # Example implementation
-    lob_data = LOBObserver(file_name="./data/book_depth_socket_btcusdt_2021_06_21.txt",
-                           past_n=10,
-                           future_n=20)
+    pth = 'C:\\Users\\auth\\projects\\python\\reinforcement learning\\RLOptimalTradeExecution\\src\\data\\book_depth_socket_btcusdt_2021_06_21.txt'
+    lob_feed = LOBSimulator(file_name=pth,
+                            nr_of_lobs=5,
+                            nr_of_lob_levels=20)
 
+    # test the functionality of this as used in the gym environment...
+    depth = 5
+    lob_hist = lob_feed.lob_snapshot(past_n=3, future_n=2, depth=depth)
+    next_lob_1 = lob_feed.next_lob_snapshot(depth=depth)
+    next_lob_2 = lob_feed.next_lob_snapshot(depth=depth)
+    next_lob_3 = lob_feed.next_lob_snapshot(depth=depth)
