88741410e769d23e006c1bb509ef2f2fcacaebb5
authc <auth@zhaw.ch>
2021-07-23 23:38:01 +0200

    Adjusting broker class to only facilitate trades given LOB

--- src/core/environment/broker.py
+++ src/core/environment/broker.py
@@ -1,4 +1,3 @@
-import copy
 import numpy as np
 from abc import ABC
 
@@ -14,40 +13,19 @@ def calc_volume_weighted_price_from_trades(trades):
 
 
 class Broker(ABC):
-    """ Can currently only compare two different orders on the same order book """
+    """ Currently only for placing trades and getting volume weighted execution prices """
 
-    def __init__(self):
-        self.algo_1 = []
-        self.algo_2 = []
+    def place_order(self, lob, order):
 
-    def place_orders(self, LOB, order_1, order_2):
-
-        # Perform trades on orginial and copy of order book
-        LOB_copy = copy.deepcopy(LOB)
-
-        if order_1['quantity'] <= 0:
-            vol_wgt_price_1, vol_1 = 0, 0
+        # Perform trades on order book
+        if order['quantity'] <= 0:
+            vol_wgt_price, vol = 0, 0
         else:
-            trades_1, order_id_1 = LOB.process_order(order_1, False, False)
-            vol_wgt_price_1, vol_1 = calc_volume_weighted_price_from_trades(trades_1)
+            trades, _ = lob.process_order(order, False, False)
+            if trades:
+                vol_wgt_price, vol = calc_volume_weighted_price_from_trades(trades)
+            else:
+                vol_wgt_price, vol = 0, 0
 
-        if order_2['quantity'] <= 0:
-            vol_wgt_price_2, vol_2 = 0, 0
-        else:
-            trades_2, order_id_2 = LOB_copy.process_order(order_2, False, False)
-            vol_wgt_price_2, vol_2 = calc_volume_weighted_price_from_trades(trades_2)
-
-        self.algo_1.append(vol_wgt_price_1)
-        self.algo_2.append(vol_wgt_price_2)
-        return vol_wgt_price_1, vol_1, vol_wgt_price_2, vol_2
-
-    def get_twaps(self):
-        if self.algo_1:
-            m_1 = np.mean(self.algo_1)
-        else:
-            m_1 = 0
-        if self.algo_2:
-            m_2 = np.mean(self.algo_2)
-        else:
-            m_2 = 0
-        return m_1, m_2
+        out_dict = {'LOB': lob, 'Execution Price': vol_wgt_price, 'Volume Traded': vol}
+        return out_dict
