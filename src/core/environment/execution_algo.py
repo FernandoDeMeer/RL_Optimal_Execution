b508610076f5e9f2d8ace66ea2e93e90f4ef0874
authc <auth@zhaw.ch>
2021-07-23 23:40:07 +0200

    Update execution algo slightly to be able to instantiate empty class

--- src/core/environment/execution_algo.py
+++ src/core/environment/execution_algo.py
@@ -5,21 +5,35 @@ from decimal import Decimal
 class ExecutionAlgo(ABC):
 
     @abstractmethod
-    def __init__(self, trade_direction, quantity, time_steps):
+    def __init__(self, trade_direction=None, quantity=None, time_steps=None):
         self.trade_direction = trade_direction
         self.quantity = quantity
         self.time_steps = time_steps
+        self.qty_remaining = quantity
         self.placed_orders = []
 
     @abstractmethod
-    def get_order_at_time(self, step):
+    def set_base_parameters(self, trade_direction, quantity, time_steps):
+        self.trade_direction = trade_direction
+        self.quantity = quantity
+        self.time_steps = time_steps
+        self.qty_remaining = quantity
+
+    @abstractmethod
+    def get_order_at_time(self, step, trade_id=None):
         NotImplementedError
+
+    def update_remainng_volume(self, volume):
+        self.qty_remaining -= volume
 
 
 class TWAPAlgo(ExecutionAlgo):
 
-    def __init__(self, trade_direction, quantity, time_steps):
+    def __init__(self, trade_direction=None, quantity=None, time_steps=None):
         super(TWAPAlgo, self).__init__(trade_direction, quantity, time_steps)
+
+    def set_base_parameters(self, trade_direction, quantity, time_steps):
+        super(TWAPAlgo, self).set_base_parameters(trade_direction, quantity, time_steps)
         self.volume_at_time = self.quantity / self.time_steps
 
     def get_order_at_time(self, step, trade_id=None):
