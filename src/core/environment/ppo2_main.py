3580470921cfb32cdc88f0a1ad4baa0d62cce646
authc <auth@zhaw.ch>
2021-07-23 23:42:22 +0200

    Example Implementation of the current environment

--- src/core/environment/ppo2_main.py
+++ src/core/environment/ppo2_main.py
@@ -1,21 +1,48 @@
+import argparse
+import gym
+import numpy as np
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines import PPO2
 from src.core.environment.broker import Broker
 from src.core.environment.execution_algo import TWAPAlgo
-from src.core.environment.lob_env_old import LimitOrderBookEnv
+from src.core.environment.base_env import BaseEnv
+from src.core.environment.lob_simulator import LOBSimulator
 
 
 if __name__ == '__main__':
 
-    # create an environment
-    lob_env = LimitOrderBookEnv(data_directory="./data/book_depth_socket_btcusdt_2021_06_21.txt",
-                                time_steps=50,
-                                trade_direction=1,
-                                qty_to_trade=3,
-                                benchmark=TWAPAlgo(trade_direction=1,
-                                                   quantity=3,
-                                                   time_steps=50),
-                                broker=Broker())
+    # construct a data feed (to be replaced by Florins implementation)
+    pth = 'C:\\Users\\auth\\projects\\python\\reinforcement learning\\RLOptimalTradeExecution\\src\\data\\book_depth_socket_btcusdt_2021_06_21.txt'
+    lob_feed = LOBSimulator(file_name=pth,
+                            nr_of_lobs=5,
+                            nr_of_lob_levels=20)
+
+    benchmark = TWAPAlgo()  # define benchmark algo
+    broker = Broker()  # set up a broker class
+    volume = 3  # total volume to trade
+    trade_steps = 50  # total number of time steps available to trade
+
+    # define observation config
+    observation_space_config = {}
+    observation_space_config['lob_depth'] = 20
+    observation_space_config['nr_of_lobs'] = 1
+    observation_space_config['norm'] = True
+
+    # define action space
+    action_space = gym.spaces.Box(low=0.0,
+                                  high=2 * volume/trade_steps,
+                                  shape= (1,),
+                                  dtype=np.float32)
+
+    # construct the environment
+    lob_env = BaseEnv(data_feed=lob_feed,
+                      trade_direction=1,
+                      qty_to_trade=3,
+                      max_step_range=50,
+                      benchmark_algo=benchmark,
+                      broker=broker,
+                      obs_config=observation_space_config,
+                      action_space=action_space)
 
     # randomly loop through the environment
     for t in range(100):
@@ -31,6 +58,7 @@ if __name__ == '__main__':
 
     # let the trained model step through the environment...
     obs = lob_env.reset()
-    while True:
+    dones = False
+    while dones is False:
         action, _states = model.predict(obs)
         obs, rewards, dones, info = lob_env.step(action)
