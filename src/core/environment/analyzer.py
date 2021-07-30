249292ff6f9d596d253785f1155156a3d9b26859
authc <auth@zhaw.ch>
2021-07-30 14:12:26 +0200

    Correct analyzer

--- src/core/environment/analyzer.py
+++ src/core/environment/analyzer.py
@@ -14,18 +14,15 @@ class DataAnalyzer:
     def calc_vwaps(self):
         vwaps = dict()
         for algo_id in self.algo_ids:
-            vwap = np.sum((self.data[algo_id]['pxs'] * self.data[algo_id]['qty']) /
-                          np.sum(self.data[algo_id]['qty']))
-            vwaps[algo_id] = vwap
+            prices = np.array(self.data[algo_id]['pxs'])
+            volumes = np.array(self.data[algo_id]['qty'])
+            vwaps[algo_id] = np.sum(prices * volumes) / np.sum(volumes)
         return vwaps
 
     def calc(self):
         pass
 
     def reset(self):
-
         self.data = {}
         for algo_id in self.algo_ids:
-            self.data[algo_id] = {"pxs" : []}
-            self.data[algo_id] = {"qty" : []}
-            self.data[algo_id] = {"t" : []}
+            self.data[algo_id] = {"pxs": [], "qty": [], "t": []}
