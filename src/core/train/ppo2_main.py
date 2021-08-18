9be6e721562773e3332b5e70d1dedd799fe23250
authc <auth@zhaw.ch>
2021-08-18 08:40:10 +0200

    stable baseline PPO with custom network implementation

--- src/core/train/ppo2_main.py
+++ src/core/train/ppo2_main.py
@@ -1,10 +1,20 @@
 import gym
 import numpy as np
-from stable_baselines.common.policies import MlpPolicy
+import tensorflow as tf
 from stable_baselines import PPO2
 from src.core.environment.execution_algo import TWAPAlgo
 from src.core.environment.base_env import BaseEnv
 from src.data.historical_data_feed import HistFeedRL
+from stable_baselines.common.policies import LstmPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.common.schedules import LinearSchedule
+
+
+class CustomLSTMPolicy(LstmPolicy):
+    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, act_fun=tf.nn.relu, n_lstm=128, reuse=False, **_kwargs):
+        super().__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm, reuse, act_fun,
+                         net_arch=[128, 128, 'lstm'],
+                         layer_norm=True, feature_extraction="mlp", **_kwargs)
 
 
 if __name__ == '__main__':
@@ -20,25 +30,27 @@ if __name__ == '__main__':
     benchmark = TWAPAlgo()  # define benchmark algo
     volume = 3  # total volume to trade
     trade_steps = 50  # total number of time steps available to trade
+    trade_direction = 1
 
     # define observation config
-    observation_space_config = {'lob_depth': 20, 'nr_of_lobs': 1, 'norm': True}
+    observation_space_config = {'lob_depth': 5, 'nr_of_lobs': 1, 'norm': True}
 
     # define action space
     action_space = gym.spaces.Box(low=0.0,
-                                  high=2 * volume/trade_steps,
+                                  high=1.0,
                                   shape=(1,),
                                   dtype=np.float32)
 
     # construct the environment
     lob_env = BaseEnv(data_feed=lob_feed,
-                      trade_direction=1,
-                      qty_to_trade=0.3,
-                      max_step_range=5,
+                      trade_direction=trade_direction,
+                      qty_to_trade=volume,
+                      max_step_range=trade_steps,
                       benchmark_algo=benchmark,
                       obs_config=observation_space_config,
                       action_space=action_space)
 
+    """
     # randomly loop through the environment
     for t in range(100):
         action = lob_env.action_space.sample()
@@ -46,14 +58,25 @@ if __name__ == '__main__':
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
+    """
+
+    # Create and wrap the environment
+    env = DummyVecEnv([lambda: lob_env])
+
+    time_steps = 10000000
+    # learning_schedule = LinearSchedule(time_steps, 0.00005, 0.00001)
+    learning_schedule = 0.0001
 
     # Train a PPO2 agent on this...
-    model = PPO2(MlpPolicy, lob_env, verbose=1, tensorboard_log="./log/")
-    model.learn(total_timesteps=30000, tb_log_name="first_trial")
+    model = PPO2(CustomLSTMPolicy, env, verbose=1, tensorboard_log="./log/",
+                 gamma=1.0, n_steps=240, ent_coef=0.01,
+                 learning_rate=learning_schedule,
+                 vf_coef=1, nminibatches=1, cliprange=0.2)
+    model.learn(total_timesteps=time_steps, tb_log_name="LSTM_Trial")
 
     # let the trained model step through the environment...
-    obs = lob_env.reset()
+    obs = env.reset()
     dones = False
     while dones is False:
         action, _states = model.predict(obs)
-        obs, rewards, dones, info = lob_env.step(action)
+        obs, rewards, dones, info = env.step(action)
