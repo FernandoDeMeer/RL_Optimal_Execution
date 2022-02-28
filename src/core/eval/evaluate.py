a1444acac12ae53e5fb3f9ffce81c625034d60c6
De Meer Pardo Fernando (demp) <demp@zhaw.ch>
2022-02-28 15:01:56 +0100

    Evaluation code
    Added function to get the best checkpoint from a given session and fixed the RNN states bug in eval_agent().

--- src/core/eval/evaluate.py
+++ src/core/eval/evaluate.py
@@ -1,8 +1,22 @@
 import numpy as np
+import os
+from os.path import isdir, isfile, join
+import ray
+from ray import tune
+from ray.tune.registry import register_env
+from ray.rllib.models import ModelCatalog
+
 from tqdm import tqdm
+from train_app import lob_env_creator, test_agent_one_episode, init_arg_parser, config, ROOT_DIR, DATA_DIR
+from ray.rllib.agents.ppo import PPOTrainer
+
+from src.data.historical_data_feed import HistoricalDataFeed
+from src.core.environment.limit_orders_setup.broker_real import Broker
+from src.core.environment.limit_orders_setup.base_env_real import DollarRewardAtStepEnv
+from src.core.agent.ray_model import CustomRNNModel
 
 
-def eval_agents(trainer, env, nr_episodes, plot=True):
+def eval_agent(trainer, env, nr_episodes, plot=True):
 
     reward_vec = []
     vwap_bmk = []
@@ -12,8 +26,9 @@ def eval_agents(trainer, env, nr_episodes, plot=True):
         obs = env.reset()
         episode_reward = 0
         done = False
+        state = trainer.get_policy().get_initial_state()
         while not done:
-            action = trainer.compute_action(obs)
+            action, state, _ = trainer.compute_action(obs,state = state)
             obs, reward, done, info = env.step(action)
             episode_reward += reward
 
@@ -38,7 +53,7 @@ def eval_agents(trainer, env, nr_episodes, plot=True):
     if plot:
         import matplotlib.pyplot as plt
 
-        fig, axs = plt.subplots(2, 2)
+        fig, axs = plt.subplots(2, 2, figsize=(12,8))
         axs[0, 0].hist(d_out['rewards'], density=True, bins=50)
         axs[0, 0].set_title('Rewards from RL Agent')
         axs[0, 0].set(xlabel='Reward', ylabel='Probability')
@@ -54,3 +69,46 @@ def eval_agents(trainer, env, nr_episodes, plot=True):
         plt.show()
 
     return d_out, stats
+
+
+def get_session_best_checkpoint_path(session_path, session,):
+
+    session_path = session_path + r'\{}\PPO'.format(str(session))
+    session_filename = [f for f in os.listdir(session_path) if isdir(join(session_path, f))]
+    sessions_path = session_path + r'\{}'.format(session_filename[0])
+
+    analysis = tune.Analysis(sessions_path)  # can also be the result of `tune.run()`
+
+    trial_logdir = analysis.get_best_logdir(metric="episode_reward_mean", mode="max")  # Can also just specify trial dir directly
+
+    # checkpoints = analysis.get_trial_checkpoints_paths(trial_logdir)  # Returns tuples of (logdir, metric)
+    best_checkpoint = analysis.get_best_checkpoint(trial_logdir, metric="episode_reward_mean", mode="max")
+
+    return best_checkpoint
+
+if __name__ == "__main__":
+
+    args = init_arg_parser()
+
+    # For debugging the ENV or other modules, set local_mode=True
+    ray.init(num_cpus=args.num_cpus,
+             local_mode=True,
+             )
+
+    sessions_path = ROOT_DIR + r'\data\sessions'
+    sessions = [int(session_id) for session_id in os.listdir(sessions_path) if session_id !='.gitignore']
+    checkpoint = get_session_best_checkpoint_path(session_path=sessions_path, session= np.max(sessions))
+
+    config["env_config"]["train_config"]["train"] = False
+    config["num_workers"] = 0
+    register_env("lob_env", lob_env_creator)
+    ModelCatalog.register_custom_model("end_to_end_model", CustomRNNModel)
+
+
+    agent = PPOTrainer(config=config)
+    agent.restore(checkpoint)
+
+    env = lob_env_creator(env_config= config["env_config"])
+    eval_agent(trainer= agent,env= env ,nr_episodes= 100, plot=True)
+
+
--- train_app.py
+++ train_app.py
@@ -59,15 +59,194 @@ def init_arg_parser():
     parser.add_argument(
         "--nr_episodes",
         type=int,
-        default=1000000,
+        default=50,
         help="Number of episodes to train.")
 
     return parser.parse_args()
 
+args = init_arg_parser()
+
+# Synchronous PPO
+config = {
+    "env": args.env,
+    "framework": args.framework,
+    # Number of GPUs to allocate to the trainer process. Note that not all
+    # algorithms can take advantage of trainer GPUs. This can be fractional
+    # (e.g., 0.3 GPUs).
+    "num_gpus": 0,
+    "num_workers": args.num_cpus - 1,
+    "num_envs_per_worker": 1,
+    # Size of batches collected from each worker.
+    "rollout_fragment_length": 200,
+    # Number of timesteps collected for each SGD round. This defines the size
+    # of each SGD epoch.
+    # DEFAULT train_batch_size: 4000
+    "train_batch_size": 1024,
+    # Total SGD batch size across all devices for SGD. This defines the
+    # minibatch size within each epoch.
+    "sgd_minibatch_size": 32,
+    # Number of SGD iterations in each outer loop (i.e., number of epochs to
+    # execute per train batch).
+    "num_sgd_iter": 30,
+    "lr": 5e-5,
+    "entropy_coeff": 0.01,
+    "lambda": 0.95,
+    "kl_coeff": 0.5,
+    "clip_param": 0.1,
+    "vf_share_layers": False,
+    "model": {
+        "custom_model": "end_to_end_model",
+        "custom_model_config": {"fcn_depth": 128,
+                                "lstm_cells": 256},
+    },
+
+    "env_config": {"obs_config": {
+        "lob_depth": 5,
+        "nr_of_lobs": 5,
+        "norm": True},
+        "train_config": {
+            "train": True,
+            "symbol": args.symbol,
+            "train_data_periods": [2021, 6, 21, 2021, 6, 21],
+            "eval_data_periods": [2021, 6, 22, 2021, 6, 22]
+        },
+        "trade_config": {"trade_direction": 1,
+                         "vol_low": 500,
+                         "vol_high": 500,
+                         "no_slices_low": 4,
+                         "no_slices_high": 4,
+                         "bucket_func": lambda no_of_slices: [0.2, 0.4, 0.6, 0.8],
+                         "rand_bucket_low": 0,
+                         "rand_bucket_high": 0},
+        "start_config": {"hour_low": 12,
+                         "hour_high": 12,
+                         "minute_low": 0,
+                         "minute_high": 0,
+                         "second_low": 0,
+                         "second_high": 0},
+        "exec_config": {"exec_times": [10]}},
+
+    # Eval
+    "evaluation_interval": 10,
+    # Number of episodes to run per evaluation period.
+    "evaluation_num_episodes": 1,
+    "evaluation_config": {
+        "explore": False,
+        "render_env": True,
+    },
+    "log_level": "WARN",
+}
+
+# ## Asynchronous PPO/IMPALA
+# config = impala.ImpalaTrainer.merge_trainer_configs(
+#     impala.DEFAULT_CONFIG,  # See keys in impala.py, which are also supported.
+#     {
+#         # Whether to use V-trace weighted advantages. If false, PPO GAE
+#         # advantages will be used instead.
+#         "vtrace": True,
+#
+#         # == These two options only apply if vtrace: False ==
+#         # Should use a critic as a baseline (otherwise don't use value
+#         # baseline; required for using GAE).
+#         "use_critic": True,
+#         # If true, use the Generalized Advantage Estimator (GAE)
+#         # with a value function, see https://arxiv.org/pdf/1506.02438.pdf.
+#         "use_gae": True,
+#         # GAE(lambda) parameter
+#         "lambda": 1.0,
+#
+#         # == PPO surrogate loss options ==
+#         "clip_param": 0.4,
+#
+#         # == PPO KL Loss options ==
+#         "use_kl_loss": False,
+#         "kl_coeff": 1.0,
+#         "kl_target": 0.01,
+#
+#         # == IMPALA optimizer params (see documentation in impala.py) ==
+#         "rollout_fragment_length": 50,
+#         "train_batch_size": 500,
+#         "min_iter_time_s": 10,
+#         "num_workers": args.num_cpus - 1,
+#         "num_gpus": 0,
+#         "num_multi_gpu_tower_stacks": 1,
+#         "minibatch_buffer_size": 1,
+#         "num_sgd_iter": 1,
+#         "replay_proportion": 0.0,
+#         "replay_buffer_num_slots": 100,
+#         "learner_queue_size": 16,
+#         "learner_queue_timeout": 300,
+#         "max_sample_requests_in_flight_per_worker": 2,
+#         "broadcast_interval": 1,
+#         "grad_clip": 40.0,
+#         "opt_type": "adam",
+#         "lr": 0.0005,
+#         "lr_schedule": None,
+#         # "lr_schedule": [[0, 0.01],
+#         #                 [3000000, 0.01]],
+#         "decay": 0.99,
+#         "momentum": 0.0,
+#         "epsilon": 0.1,
+#         "vf_loss_coeff": 0.5,
+#         "entropy_coeff": 0.01,
+#         "entropy_coeff_schedule": None,
+#
+#         "env": "lob_env",
+#         "env_config": {
+#             "obs_config": {
+#                 "lob_depth": 5,
+#                 "nr_of_lobs": 5,
+#                 "norm": True},
+#             "train_config": {
+#                 "train": True,
+#                 "symbol": args.symbol,
+#                 "train_data_periods": [2021, 6, 5, 2021, 6, 11],
+#                 "eval_data_periods": [2021, 6, 12, 2021, 6, 14]
+#             },
+#             "trade_config": {"trade_direction": 1,
+#                              "vol_low": 100,
+#                              "vol_high": 300,
+#                              "no_slices_low": 4,
+#                              "no_slices_high": 4,
+#                              "bucket_func": lambda no_of_slices: [0.2, 0.4, 0.6, 0.8],
+#                              "rand_bucket_low": 0,
+#                              "rand_bucket_high": 0},
+#             "start_config": {"hour_low": 1,
+#                              "hour_high": 22,
+#                              "minute_low": 0,
+#                              "minute_high": 59,
+#                              "second_low": 0,
+#                              "second_high": 0},
+#             "exec_config": {"exec_times": [5, 10, 15],
+#                             "delete_vol": False},
+#             'reset_config': {'reset_num_episodes': 500 * 10,
+#                              'samples_per_feed': 500,
+#                              'reset_feed': True},
+#         },
+#
+#         # Eval
+#         "evaluation_interval": 10,
+#         # Number of episodes to run per evaluation period.
+#         "evaluation_num_episodes": 1,
+#         "evaluation_config": {
+#             "explore": False,
+#             "render_env": True,
+#         },
+#         "model": {
+#             "custom_model": "end_to_end_model",
+#             "custom_model_config": {"fcn_depth": 128,
+#                                     "lstm_cells": 256},
+#         },
+#         "log_level": "WARN",
+#     },
+#     _allow_unknown_configs=True,
+# )
 
 def lob_env_creator(env_config):
-
-    is_env_eval = env_config.num_workers == 0
+    try:
+        is_env_eval = env_config.num_workers == 0
+    except:
+        is_env_eval = True
 
     if is_env_eval:
         data_periods = env_config["train_config"]["eval_data_periods"]
@@ -115,19 +294,7 @@ def test_agent_one_episode(config, agent_path, eval_data_periods, symbol):
     agent = PPOTrainer(config=config)
     agent.restore(agent_path)
 
-    class DummyCfg:
-        def __init__(self):
-            self.num_workers = 0
-
-        def __getitem__(self, key):
-
-            if key == "eval_data_periods":
-                return eval_data_periods
-
-            elif key == "symbol":
-                return symbol
-
-    env = lob_env_creator(DummyCfg())
+    env = lob_env_creator(config)
 
     episode_reward = 0
     done = False
@@ -142,7 +309,6 @@ def test_agent_one_episode(config, agent_path, eval_data_periods, symbol):
 
 if __name__ == "__main__":
 
-    args = init_arg_parser()
 
     # For debugging the ENV or other modules, set local_mode=True
     ray.init(num_cpus=args.num_cpus,
@@ -153,191 +319,13 @@ if __name__ == "__main__":
     register_env("lob_env", lob_env_creator)
     ModelCatalog.register_custom_model("end_to_end_model", CustomRNNModel)
 
-    """
-    Synchronous PPO
-    config = {
-        "env": args.env,
-        "framework": args.framework,
-        # Number of GPUs to allocate to the trainer process. Note that not all
-        # algorithms can take advantage of trainer GPUs. This can be fractional
-        # (e.g., 0.3 GPUs).
-        "num_gpus": 0,
-        "num_workers": args.num_cpus - 1,
-        "num_envs_per_worker": 1,
-        # Size of batches collected from each worker.
-        "rollout_fragment_length": 200,
-        # Number of timesteps collected for each SGD round. This defines the size
-        # of each SGD epoch.
-        # DEFAULT train_batch_size: 4000
-        "train_batch_size": 1024,
-        # Total SGD batch size across all devices for SGD. This defines the
-        # minibatch size within each epoch.
-        "sgd_minibatch_size": 32,
-        # Number of SGD iterations in each outer loop (i.e., number of epochs to
-        # execute per train batch).
-        "num_sgd_iter": 30,
-        "lr": 5e-5,
-        "entropy_coeff": 0.01,
-        "lambda": 0.95,
-        "kl_coeff": 0.5,
-        "clip_param": 0.1,
-        "vf_share_layers": False,
-        "model": {
-            "custom_model": "end_to_end_model",
-            "custom_model_config": {"fcn_depth": 128,
-                                    "lstm_cells": 256},
-        },
-
-        "env_config": {"obs_config": {
-            "lob_depth": 5,
-            "nr_of_lobs": 5,
-            "norm": True},
-            "train_config": {
-                "train": True,
-                "symbol": args.symbol,
-                "train_data_periods": [2021, 6, 21, 2021, 6, 21],
-                "eval_data_periods": [2021, 6, 22, 2021, 6, 22]
-            },
-            "trade_config": {"trade_direction": 1,
-                             "vol_low": 500,
-                             "vol_high": 500,
-                             "no_slices_low": 4,
-                             "no_slices_high": 4,
-                             "bucket_func": lambda no_of_slices: [0.2, 0.4, 0.6, 0.8],
-                             "rand_bucket_low": 0,
-                             "rand_bucket_high": 0},
-            "start_config": {"hour_low": 12,
-                             "hour_high": 12,
-                             "minute_low": 0,
-                             "minute_high": 0,
-                             "second_low": 0,
-                             "second_high": 0},
-            "exec_config": {"exec_times": [10]}},
-
-        # Eval
-        "evaluation_interval": 10,
-        # Number of episodes to run per evaluation period.
-        "evaluation_num_episodes": 1,
-        "evaluation_config": {
-            "explore": False,
-            "render_env": True,
-        },
-        "log_level": "WARN",
-    }
-    """
-
-    ## Asynchronous PPO/IMPALA
-    config = impala.ImpalaTrainer.merge_trainer_configs(
-        impala.DEFAULT_CONFIG,  # See keys in impala.py, which are also supported.
-        {
-            # Whether to use V-trace weighted advantages. If false, PPO GAE
-            # advantages will be used instead.
-            "vtrace": True,
-
-            # == These two options only apply if vtrace: False ==
-            # Should use a critic as a baseline (otherwise don't use value
-            # baseline; required for using GAE).
-            "use_critic": True,
-            # If true, use the Generalized Advantage Estimator (GAE)
-            # with a value function, see https://arxiv.org/pdf/1506.02438.pdf.
-            "use_gae": True,
-            # GAE(lambda) parameter
-            "lambda": 1.0,
-
-            # == PPO surrogate loss options ==
-            "clip_param": 0.4,
-
-            # == PPO KL Loss options ==
-            "use_kl_loss": False,
-            "kl_coeff": 1.0,
-            "kl_target": 0.01,
-
-            # == IMPALA optimizer params (see documentation in impala.py) ==
-            "rollout_fragment_length": 50,
-            "train_batch_size": 500,
-            "min_iter_time_s": 10,
-            "num_workers": args.num_cpus - 1,
-            "num_gpus": 0,
-            "num_multi_gpu_tower_stacks": 1,
-            "minibatch_buffer_size": 1,
-            "num_sgd_iter": 1,
-            "replay_proportion": 0.0,
-            "replay_buffer_num_slots": 100,
-            "learner_queue_size": 16,
-            "learner_queue_timeout": 300,
-            "max_sample_requests_in_flight_per_worker": 2,
-            "broadcast_interval": 1,
-            "grad_clip": 40.0,
-            "opt_type": "adam",
-            "lr": 0.0005,
-            "lr_schedule": None,
-            # "lr_schedule": [[0, 0.01],
-            #                 [3000000, 0.01]],
-            "decay": 0.99,
-            "momentum": 0.0,
-            "epsilon": 0.1,
-            "vf_loss_coeff": 0.5,
-            "entropy_coeff": 0.01,
-            "entropy_coeff_schedule": None,
-
-            "env": "lob_env",
-            "env_config": {
-                "obs_config": {
-                    "lob_depth": 5,
-                    "nr_of_lobs": 5,
-                    "norm": True},
-                "train_config": {
-                    "train": True,
-                    "symbol": args.symbol,
-                    "train_data_periods": [2021, 6, 5, 2021, 6, 11],
-                    "eval_data_periods": [2021, 6, 12, 2021, 6, 14]
-                },
-                "trade_config": {"trade_direction": 1,
-                                 "vol_low": 100,
-                                 "vol_high": 300,
-                                 "no_slices_low": 4,
-                                 "no_slices_high": 4,
-                                 "bucket_func": lambda no_of_slices: [0.2, 0.4, 0.6, 0.8],
-                                 "rand_bucket_low": 0,
-                                 "rand_bucket_high": 0},
-                "start_config": {"hour_low": 1,
-                                 "hour_high": 22,
-                                 "minute_low": 0,
-                                 "minute_high": 59,
-                                 "second_low": 0,
-                                 "second_high": 0},
-                "exec_config": {"exec_times": [5, 10, 15],
-                                "delete_vol": False},
-                'reset_config': {'reset_num_episodes': 500 * 10,
-                                 'samples_per_feed': 500,
-                                 'reset_feed': True},
-            },
-
-            # Eval
-            "evaluation_interval": 10,
-            # Number of episodes to run per evaluation period.
-            "evaluation_num_episodes": 1,
-            "evaluation_config": {
-                "explore": False,
-                "render_env": True,
-            },
-            "model": {
-                "custom_model": "end_to_end_model",
-                "custom_model_config": {"fcn_depth": 128,
-                                        "lstm_cells": 256},
-            },
-            "log_level": "WARN",
-        },
-        _allow_unknown_configs=True,
-    )
-
     ##
     session_container_path = init_session_container(args.session_id)
     # with open(os.path.join(session_container_path, "config.json"), "a", encoding="utf-8") as f:
     #     json.dump(config, f, ensure_ascii=False, indent=4)
 
     # PPOTrainer
-    experiment = tune.run("APPO",
+    experiment = tune.run("PPO",
                           config=config,
                           metric="episode_reward_mean",
                           mode="max",
@@ -345,17 +333,17 @@ if __name__ == "__main__":
                           stop={"training_iteration": args.nr_episodes},
                           checkpoint_at_end=True,
                           local_dir=session_container_path,
-                          max_failures=-1
+                          max_failures=0
                           )
 
-    # checkpoints = experiment.get_trial_checkpoints_paths(trial=experiment.get_best_trial("episode_reward_mean"),
-    #                                                      metric="episode_reward_mean")
-    # checkpoint_path = checkpoints[0][0]
-    #
-    # reward = test_agent_one_episode(config=config,
-    #                                 agent_path=checkpoint_path,
-    #                                 eval_data_periods=[2021, 6, 21, 2021, 6, 21],
-    #                                 symbol="btcusdt")
-    # print(reward)
+    checkpoints = experiment.get_trial_checkpoints_paths(trial=experiment.get_best_trial("episode_reward_mean"),
+                                                         metric="episode_reward_mean")
+    checkpoint_path = checkpoints[0][0]
+
+    reward = test_agent_one_episode(config=config,
+                                    agent_path=checkpoint_path,
+                                    eval_data_periods=[2021, 6, 21, 2021, 6, 21],
+                                    symbol="btcusdt")
+    print(reward)
 
     ray.shutdown()
