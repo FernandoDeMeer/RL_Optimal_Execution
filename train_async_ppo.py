e79f368d17963c68fadbcc0d27652f9d9d535905
florin <florin@home.com>
2022-03-31 12:57:23 +0200

    Async PPO

---
+++ train_async_ppo.py
@@ -0,0 +1,353 @@
+#
+#   Reinforcement Learning Optimal Trade Execution
+#   Async PPO
+#
+
+
+import os
+import time
+import datetime
+import argparse
+import math
+
+import gym
+import json
+from decimal import Decimal
+import numpy as np
+
+import ray
+from ray import tune
+from ray.tune.registry import register_env
+from ray.rllib.agents import dqn
+from ray.tune.logger import pretty_print
+from ray.rllib.agents.impala import impala
+
+from ray.rllib.agents.ppo.appo import APPOTrainer
+
+from src.data.historical_data_feed import HistoricalDataFeed
+from src.core.environment.limit_orders_setup.broker_real import Broker
+from src.core.environment.limit_orders_setup.base_env_real import BaseEnv
+
+
+class NarrowTradeLimitEnvDQN(BaseEnv):
+
+    def __init__(self, *args, **kwargs):
+        super(NarrowTradeLimitEnvDQN, self).__init__(*args, **kwargs)
+
+    def _convert_action(self, action):
+        shift = 0.2
+        if action == 0:
+            action_out = 1 - shift
+        elif action == 1:
+            action_out = 1
+        elif action == 2:
+            action_out = 1 + shift
+        else:
+            raise ValueError
+        # action_out = 1
+        return action_out
+
+    def infer_volume_from_action(self, action):
+        vol_to_trade = Decimal(str(action)) * \
+                       self.broker.benchmark_algo.volumes_per_trade[self.broker.rl_algo.bucket_idx][self.broker.benchmark_algo.order_idx]
+        factor = 10 ** (- self.broker.benchmark_algo.tick_size.as_tuple().exponent)
+        vol_to_trade = Decimal(str(math.floor(vol_to_trade * factor) / factor))
+        if vol_to_trade > self.broker.rl_algo.bucket_vol_remaining[self.broker.rl_algo.bucket_idx]:
+            vol_to_trade = self.broker.rl_algo.bucket_vol_remaining[self.broker.rl_algo.bucket_idx]
+        return vol_to_trade
+
+    #def reward_func(self):
+    #    """ Env with reward at end of each bucket as $ improvement of VWAP """
+
+    #    reward = 0
+    #    try:
+    #        if self.bucket_time != self.bucket_time_prev:
+    #            vwap_bmk, vwap_rl = self.broker.calc_vwap_from_logs(start_date=self.bucket_time_prev,
+    #                                                                end_date=self.bucket_time)
+    #            if self.trade_dir == 1:
+    #                reward = np.sign(vwap_bmk - vwap_rl)
+    #            else:
+    #                reward = np.sign(vwap_rl - vwap_bmk)
+    #    except:
+    #        reward = 0
+    #    return reward
+
+    def reward_func(self):
+        """ Env with reward at end of each bucket as $ improvement of VWAP """
+        reward = 0
+        vol = float(np.sum(self.broker.benchmark_algo.volumes_per_trade[self.bucket_idx]))
+        try:
+            if self.bucket_time != self.bucket_time_prev:
+                vwap_bmk, vwap_rl = self.broker.calc_vwap_from_logs(start_date=self.bucket_time_prev,
+                                                                end_date=self.bucket_time)
+                if self.trade_dir == 1:
+                    # reward = np.sign(vwap_bmk - vwap_rl)
+                    reward = vol * (vwap_bmk - vwap_rl)
+                else:
+                    # reward = np.sign(vwap_rl - vwap_bmk)
+                    reward = vol * (vwap_rl - vwap_bmk)
+        except:
+            reward = 0
+        return reward
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+
+def init_arg_parser():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--env", type=str, default="lob_env")
+    parser.add_argument("--num-cpus", type=int, default=1)
+
+    parser.add_argument(
+        "--framework",
+        choices=["tf", "torch"],
+        default="tf",
+        help="The DL framework specifier.")
+
+    parser.add_argument(
+        "--symbol",
+        choices=["btc_usdt"],
+        default="btcusdt",
+        help="Market symbol.")
+
+    parser.add_argument(
+        "--session_id",
+        type=str,
+        default="0",
+        help="Session id.")
+
+    parser.add_argument(
+        "--nr_episodes",
+        type=int,
+        default=10000000,
+        help="Number of episodes to train.")
+
+    parser.add_argument(
+        "--no-tune",
+        type=bool,
+        default=True,
+        help="Run without Tune using a manual train loop instead.")
+
+    parser.add_argument(
+        "--stop-timesteps",
+        type=int,
+        default=100000,
+        help="Number of timesteps to train.")
+
+    parser.add_argument(
+        "--stop-reward",
+        type=float,
+        default=0.9,
+        help="Reward at which we stop training.")
+
+    return parser.parse_args()
+
+
+def lob_env_creator(env_config):
+
+    if env_config['train_config']['train']:
+        data_periods = env_config['train_config']["train_data_periods"]
+    else:
+        data_periods = env_config['train_config']["eval_data_periods"]
+
+    data_start_day = datetime.datetime(year=data_periods[0], month=data_periods[1], day=data_periods[2])
+    data_end_day = datetime.datetime(year=data_periods[3], month=data_periods[4], day=data_periods[5])
+
+    lob_feed = HistoricalDataFeed(data_dir=os.path.join(DATA_DIR, "market", env_config['train_config']["symbol"]),
+                                  instrument=env_config['train_config']["symbol"],
+                                  start_day=data_start_day,
+                                  end_day=data_end_day)
+
+    exclude_keys = {'train_config'}
+    env_config_clean = {k: env_config[k] for k in set(list(env_config.keys())) - set(exclude_keys)}
+
+    return NarrowTradeLimitEnvDQN(broker=Broker(lob_feed),
+                                  action_space=gym.spaces.Discrete(3),
+                                  config=env_config_clean)
+
+
+def init_session_container(session_id):
+
+    if args.session_id == "0":
+        session_id = str(int(time.time()))
+
+    session_container_path = os.path.join("data", "sessions", session_id)
+
+    if not os.path.isdir(session_container_path):
+        os.makedirs(session_container_path)
+
+    return session_container_path
+
+
+# def test_agent_one_episode(config, agent_path):
+#
+#     agent = dqn.DQNTrainer(config=config)
+#     agent.restore(agent_path)
+#     config['env_config']['train_config']['train'] = False
+#     env = lob_env_creator(config['env_config'])
+#
+#     episode_reward = 0
+#     done = False
+#     obs = env.reset()
+#     while not done:
+#         action = agent.compute_action(obs)
+#         obs, reward, done, info = env.step(action)
+#         episode_reward += reward
+#
+#     return episode_reward
+
+
+if __name__ == "__main__":
+
+    args = init_arg_parser()
+
+    # For debugging the env or other modules, set local_mode=True
+    ray.init(local_mode=False, num_cpus=args.num_cpus + 1)
+    register_env(args.env, lob_env_creator)
+
+    # APPO based IMPALA CONFIG
+    APPO_CONFIG = impala.ImpalaTrainer.merge_trainer_configs(
+        impala.DEFAULT_CONFIG,  # See keys in impala.py, which are also supported.
+        {
+            ####
+            "framework": args.framework,
+            "env": args.env,
+
+            # Whether to use V-trace weighted advantages. If false, PPO GAE
+            # advantages will be used instead.
+            "vtrace": True,
+
+            # == These two options only apply if vtrace: False ==
+            # Should use a critic as a baseline (otherwise don't use value
+            # baseline; required for using GAE).
+            "use_critic": True,
+            # If true, use the Generalized Advantage Estimator (GAE)
+            # with a value function, see https://arxiv.org/pdf/1506.02438.pdf.
+            "use_gae": True,
+            # GAE(lambda) parameter
+            "lambda": 1.0,
+
+            # == PPO surrogate loss options ==
+            "clip_param": 0.4,
+
+            # == PPO KL Loss options ==
+            "use_kl_loss": False,
+            "kl_coeff": 1.0,
+            "kl_target": 0.01,
+
+            # == IMPALA optimizer params (see documentation in impala.py) ==
+            "rollout_fragment_length": 50,
+            "train_batch_size": 500,
+            # "min_time_s_per_reporting": 10,
+            ####
+            "num_workers": args.num_cpus,
+            "num_gpus": 0,
+            "num_envs_per_worker": 1,
+            "num_multi_gpu_tower_stacks": 1,
+            "minibatch_buffer_size": 1,
+            "num_sgd_iter": 1,
+            "replay_proportion": 0.0,
+            "replay_buffer_num_slots": 100,
+            "learner_queue_size": 16,
+            "learner_queue_timeout": 300,
+            "max_sample_requests_in_flight_per_worker": 2,
+            "broadcast_interval": 1,
+            "grad_clip": 40.0,
+            "opt_type": "adam",
+            "lr": 0.0005,
+            "lr_schedule": None,
+            "decay": 0.99,
+            "momentum": 0.0,
+            "epsilon": 0.1,
+            "vf_loss_coeff": 0.5,
+            "entropy_coeff": 0.01,
+            "entropy_coeff_schedule": None,
+
+            ####
+            # "evaluation_interval": 10,
+            # "train_batch_size": 200,
+            # # Number of episodes to run per evaluation period.
+            # "evaluation_num_episodes": 1,
+            # "evaluation_config": {
+            #     "explore": False,
+            #     "render_env": True,
+            # },
+        },
+        _allow_unknown_configs=True,
+    )
+
+    # Add config for our custom environment
+    env_config = {'obs_config': {"lob_depth": 5,
+                                 "nr_of_lobs": 5,
+                                 "norm": True},
+                  "train_config": {
+                      "train": True,
+                      "symbol": 'btcusdt',
+                      "train_data_periods": [2021, 6, 21, 2021, 6, 21],
+                      "eval_data_periods": [2021, 6, 22, 2021, 6, 22]
+                  },
+                  'trade_config': {'trade_direction': 1,
+                                   'vol_low': 100,
+                                   'vol_high': 100,
+                                   'no_slices_low': 3,
+                                   'no_slices_high': 3,
+                                   'bucket_func': lambda no_of_slices: [0.25, 0.5, 0.75],
+                                   'rand_bucket_low': 0,
+                                   'rand_bucket_high': 0},
+                  'start_config': {'hour_low': 1,
+                                   'hour_high': 23,
+                                   'minute_low': 0,
+                                   'minute_high': 0,
+                                   'second_low': 0,
+                                   'second_high': 0},
+                  'exec_config': {'exec_times': [5],
+                                  'delete_vol': False},
+                  'reset_config': {'reset_num_episodes': 30,
+                                   'samples_per_feed': 2000,
+                                   'reset_feed': True},
+                  'seed_config': {'seed': 0,}}
+    env_config = {"env_config": env_config}
+    APPO_CONFIG.update(env_config)
+
+    # config for stopping the training
+    stop = {
+        "training_iteration": args.nr_episodes,
+        "timesteps_total": args.stop_timesteps,
+        "episode_reward_mean": args.stop_reward,
+    }
+
+    session_container_path = init_session_container(args.session_id)
+
+    no_tune = False
+    if no_tune:
+
+        # dqn_config = dqn.DEFAULT_CONFIG.copy()
+        # dqn_config.update(DEFAULT_CONFIG)
+        trainer = APPOTrainer(config=APPO_CONFIG)
+
+        # run manual training loop and print results after each iteration
+        for _ in range(args.nr_episodes):
+            result = trainer.train()
+            print(pretty_print(result))
+            # stop training of the target train steps or reward are reached
+            if result["timesteps_total"] >= args.stop_timesteps or \
+                    result["episode_reward_mean"] >= args.stop_reward:
+                break
+    else:
+        # automated run with Tune and grid search and TensorBoard
+        print("Training automatically with Ray Tune")
+        results = tune.run("APPO",
+                           config=APPO_CONFIG,
+                           metric="episode_reward_mean",
+                           mode="max",
+                           checkpoint_freq=10,
+                           stop={"training_iteration": args.nr_episodes},
+                           checkpoint_at_end=True,
+                           local_dir=session_container_path,
+                           )
+
+    ray.shutdown()
